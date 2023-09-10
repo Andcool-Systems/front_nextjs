bed16a2cdb2d3aabfb1aecfde7ffddf5cdc4e6ea
andcool-2007 <drolll2007@gmail.com>
2023-09-10 22:41:44 +0300

    fixed votes page

--- src/app/votes/page.tsx
+++ src/app/votes/page.tsx
@@ -2,23 +2,42 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 import { loadVotes } from "./login.tsx"
+import React from 'react';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from 'react-query';
+
+const queryClient = new QueryClient();
+
+async function fetchVotes(){
+	const data = await loadVotes();
+	return data;
+}
 
 export default function Home() {
-    const votes = loadVotes().then((votes) => {
-        
-        var listItems = votes.map(person =>
-            
-            <li key={person.id}>
-              <p>
-                <b>{person.name}</b>
-                  {' ' + person.profession + ' '}
-                  known for {person.accomplishment}
-              </p>
-            </li>
-        );
-        console.log(votes);
-        var ul = document.getElementById("votes");
-        ul.appendChild(listItems);
-      })
-      return <ul id="votes"></ul>;
+
+    return (
+		<QueryClientProvider client={queryClient}>
+		  <Main />
+		</QueryClientProvider>
+	  );
+}
+
+function Main(){
+	const { data, isLoading, isError} = useQuery('votes', fetchVotes);
+	if (isLoading) return <h1>Загрузка...</h1>
+	if (isError) return <h1>Ошибка...</h1>
+	if (!data) return <h1>Нет данных...</h1>
+	const listItems = data.map(person =>
+		<li key={person.id}>
+		  <p>
+			<b>{person.name}</b>
+			  {' ' + person.profession + ' '}
+			  known for {person.accomplishment}
+		  </p>
+		</li>
+	  );
+	return <ul>{listItems}</ul>;
 }
